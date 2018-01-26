86d284024474a1115b8f37800ce1412032759f67
jeanlabab <vincent.rochatte@gmail.com>
2018-01-26 13:56:03 +0100

    4VsurS
    testé sur sphere, donne bien 1.33*r

--- Mcm3D.15juin2014/mcmCodage/mcmAlgo.h
+++ Mcm3D.15juin2014/mcmCodage/mcmAlgo.h
@@ -1,36 +1,46 @@
+//ALGO 4VsurS//
+
 #define MCM_nombreDeVariablesDuMonteCarloDeScene 2  // number of quantities evaluated simultaneously (including the proportion of failures)
-#define MCM_nombreDeParametresDuMonteCarloDeScene 3 // number of parameters (we evaluate the sensitivity of each quantity with respect to each parameter)
+#define MCM_nombreDeParametresDuMonteCarloDeScene 0 // number of parameters (we evaluate the sensitivity of each quantity with respect to each parameter)
 mcmVecteurSommeDeRealisations Scene::realisation() const {
 
-  mcmVecteurSommeDeRealisations vsr(dimension);  // the mcmVecteurSommeDeRealisations object returned by realisation()
-  vsr.nombreDeRealisations = 1;  // number of performed realizations each time realisation() is called
-  int failuresNumber = 0;  // number of attempted realization that failed for numerical reasons 
-  double wFailuresProportion;  // weight used for the evaluation of the failures proportion
-  double wFailuresProportion_void;  // weight used for the sensitivities of the failures proportion (which are set to zero as they aren't evaluated)
+mcmVecteurSommeDeRealisations vsr(dimension);  // the mcmVecteurSommeDeRealisations object returned by realisation()
+vsr.nombreDeRealisations = 1;  // number of performed realizations each time realisation() is called
 
-  double w;
-  double w_param1;
-  double w_param2;
-  double w_param3;
+//Photons perdus
+int nbPerdu;
+beginning:;
 
-  w=mcmRng();
-  w_param1=0.;
-  w_param2=0.;
-  w_param3=0.;
+//Suivi de rayon
+int indiceSurface=0.;
+Ray emission;
+double poids=0.;
+Ray rayonSuivi;
+Intersection impactSurface;
+double distanceSurface=0.;
 
-  vsr.initialisationUneComposante(indiceDeLaCoordonneeDuVecteurSommeDeRealisations(1),wFailuresProportion);
-  vsr.initialisationUneComposante(indiceDeLaCoordonneeDuVecteurSommeDeRealisations(1,1),wFailuresProportion_void);
-  vsr.initialisationUneComposante(indiceDeLaCoordonneeDuVecteurSommeDeRealisations(1,2),wFailuresProportion_void);
-  vsr.initialisationUneComposante(indiceDeLaCoordonneeDuVecteurSommeDeRealisations(1,3),wFailuresProportion_void);
-  vsr.initialisationUneComposante(indiceDeLaCoordonneeDuVecteurSommeDeRealisations(2),w);
-  vsr.initialisationUneComposante(indiceDeLaCoordonneeDuVecteurSommeDeRealisations(2,1),w_param1);
-  vsr.initialisationUneComposante(indiceDeLaCoordonneeDuVecteurSommeDeRealisations(2,2),w_param2);
-  vsr.initialisationUneComposante(indiceDeLaCoordonneeDuVecteurSommeDeRealisations(2,3),w_param3);
-	
-  /***************************************************************************/
-  /* Free the memory that Pbrt uses for BSDF usage (at intersection points)  */
-  /***************************************************************************/
-  BSDF::FreeAll();
 
-  return vsr; 
+//echantillonnage d'un chemin optique
+indiceSurface = mcmTirageDuneSurfaceIntegrable();//échantillonnage d'une surface d'émission
+emission = (*lights[indiceSurface]).mcmTirageUniformeLambertien(poids);//echantillonnage d'une direction d'émission
+rayonSuivi = Ray(emission.o,emission.d,RAY_EPSILON,INFINITY,0.);
+if (!Intersect(rayonSuivi,&impactSurface)) {
+	nbPerdu++;
+	if (nbPerdu>0.1*nombreDeRealisationsDemandees){printf("Trop de photons perdus, arrêt.\n");exit(0);}
+	goto beginning;
+	}
+
+else {
+	distanceSurface = Distance(rayonSuivi.o,impactSurface.dg.p);
+	}
+vsr.initialisationUneComposante(indiceDeLaCoordonneeDuVecteurSommeDeRealisations(1),distanceSurface);
+vsr.initialisationUneComposante(indiceDeLaCoordonneeDuVecteurSommeDeRealisations(2),2.);
+
+
+/***************************************************************************/
+/* Free the memory that Pbrt uses for BSDF usage (at intersection points)  */
+/***************************************************************************/
+BSDF::FreeAll();
+
+return vsr;
 }
