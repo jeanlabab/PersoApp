5a636feb410a62aebcd433d8c5098acd1cef40d6
jeanlabab <vincent.rochatte@gmail.com>
2018-02-21 15:09:36 +0100

    Corrections - non validé
    initialisation de nbPhotonsPerdus à 0
    ok pour sphère
    on trouve 0.088 au lieu de 0.091 sur Dicofluv_Jeremi.pbrt

--- Mcm3D.15juin2014/mcmCodage/mcmAlgo.h
+++ Mcm3D.15juin2014/mcmCodage/mcmAlgo.h
@@ -4,43 +4,63 @@
 #define MCM_nombreDeParametresDuMonteCarloDeScene 0 // number of parameters (we evaluate the sensitivity of each quantity with respect to each parameter)
 mcmVecteurSommeDeRealisations Scene::realisation() const {
 
-mcmVecteurSommeDeRealisations vsr(dimension);  // the mcmVecteurSommeDeRealisations object returned by realisation()
-vsr.nombreDeRealisations = 1;  // number of performed realizations each time realisation() is called
+	mcmVecteurSommeDeRealisations vsr(dimension); // the mcmVecteurSommeDeRealisations object returned by realisation()
+	vsr.nombreDeRealisations = 1; // number of performed realizations each time realisation() is called
 
 //Photons perdus
-int nbPerdu;
-beginning:;
+	int nbPerdu(0);
+	beginning: ;
 
 //Suivi de rayon
-int indiceSurface=0.;
-Ray emission;
-double poids=0.;
-Ray rayonSuivi;
-Intersection impactSurface;
-double distanceSurface=0.;
-
+	bool absorption(false);
+	int indiceSurface = 0.;
+	Ray emission;
+	double poids = 0.;
+	Ray rayonSuivi;
+	Intersection impactSurface;
+	double distanceSurface(0.);
+	Vector directionReflexion;
 
 //echantillonnage d'un chemin optique
-indiceSurface = mcmTirageDuneSurfaceIntegrable();//échantillonnage d'une surface d'émission
-emission = (*lights[indiceSurface]).mcmTirageUniformeLambertien(poids);//echantillonnage d'une direction d'émission
-rayonSuivi = Ray(emission.o,emission.d,RAY_EPSILON,INFINITY,0.);
-if (!Intersect(rayonSuivi,&impactSurface)) {
-	nbPerdu++;
-	if (nbPerdu>0.1*nombreDeRealisationsDemandees){printf("Trop de photons perdus, arrêt.\n");exit(0);}
-	goto beginning;
+	indiceSurface = mcmTirageDuneSurfaceIntegrable(); //échantillonnage d'une surface d'émission
+	emission = (*lights[indiceSurface]).mcmTirageUniformeLambertien(poids); //echantillonnage d'une direction d'émission
+	rayonSuivi = Ray(emission.o, emission.d, RAY_EPSILON, INFINITY, 0.);
+	while (!absorption) {
+		if (!Intersect(rayonSuivi, &impactSurface)) {
+			nbPerdu++;
+			if (nbPerdu > 0.1 * nombreDeRealisationsDemandees) {
+				printf("Trop de photons perdus, arrêt.\n");
+				exit(0);
+			}
+			goto beginning;
+		}
+
+		else {
+			distanceSurface += Distance(rayonSuivi.o, impactSurface.dg.p);
+			impactSurface.GetBSDF(RayDifferential(rayonSuivi))->get_mcmSurfaceAvecNumero(
+					indiceSurface);
+			if (indiceSurface == 1) {
+				impactSurface.GetBSDF(RayDifferential(rayonSuivi))->Sample_f(
+						rayonSuivi.d, &directionReflexion);
+				rayonSuivi = Ray(impactSurface.dg.p, -directionReflexion,
+						RAY_EPSILON, INFINITY, 0.);
+			}
+			if (indiceSurface == 0) {
+				absorption = true;
+			}
+		}
 	}
+	vsr.initialisationUneComposante(
+			indiceDeLaCoordonneeDuVecteurSommeDeRealisations(1),
+			distanceSurface);
+	vsr.initialisationUneComposante(
+			indiceDeLaCoordonneeDuVecteurSommeDeRealisations(2),
+			nbPerdu / (nbPerdu + 1));
 
-else {
-	distanceSurface = Distance(rayonSuivi.o,impactSurface.dg.p);
-	}
-vsr.initialisationUneComposante(indiceDeLaCoordonneeDuVecteurSommeDeRealisations(1),distanceSurface);
-vsr.initialisationUneComposante(indiceDeLaCoordonneeDuVecteurSommeDeRealisations(2),2.);
+	/***************************************************************************/
+	/* Free the memory that Pbrt uses for BSDF usage (at intersection points)  */
+	/***************************************************************************/
+	BSDF::FreeAll();
 
-
-/***************************************************************************/
-/* Free the memory that Pbrt uses for BSDF usage (at intersection points)  */
-/***************************************************************************/
-BSDF::FreeAll();
-
-return vsr;
+	return vsr;
 }
